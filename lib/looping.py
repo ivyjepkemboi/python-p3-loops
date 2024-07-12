7f152b5bb49cc32238b86f0e4bc10fa52fd45cda
Ivy <ivyjkemboi@gmail.com>
2024-07-12 22:41:23 +0300

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10 , -1, -1):
+        print  (i)
+    if i < 1:
+        print ("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = list()
+    for int in int_list:
+        sqr = int*int
+        squared.append(sqr)
+       
+    return squared
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+      
+        if i % 3 == 0 and i % 5 == 0:
+            print ("FizzBuzz")
+
+        elif i % 3 == 0:
+            print ("Fizz")
+
+        elif i % 5 == 0:
+            print ("Buzz")
+
+        else:
+            print (i)
+
+
